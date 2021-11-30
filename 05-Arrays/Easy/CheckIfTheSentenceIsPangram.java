cd6d912ba5993d735b730a9b794bfcb5da5016b6
Vicky <vickygupta9987@gmail.com>
2021-11-30 23:14:31 +0530

    solution added

---
+++ 05-Arrays/Easy/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,40 @@
+package com.company.Arrays_and_ArrayList.Assignment;
+
+/*
+//https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+*/
+
+public class CheckIfTheSentenceIsPangram {
+    public static void main(String[] args) {
+        String sentenance = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfPangram(sentenance));
+    }
+    public static boolean checkIfPangram(String sentence) {
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        for(char ch: str.toCharArray()){
+            if(sentence.indexOf(ch) == -1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 05-Arrays/Easy/CountItemsMatchingARule.java
@@ -0,0 +1,27 @@
+package com.company.Arrays_and_ArrayList.Assignment;
+
+import java.util.List;
+
+public class CountItemsMatchingARule {
+    public static void main(String[] args) {
+        List<List<String>> items = {{"phone","blue","pixel"},{"computer","silver","phone"},{"phone","gold","iphone"}};
+        String ruleKey = "type";
+        String ruleValue = "phone";
+        System.out.println(countMatches(items,ruleKey,ruleValue));
+    }
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        for(int i = 0;i<items.size();i++){
+            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)){
+                count++;
+            }
+            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)){
+                count++;
+            }
+            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
