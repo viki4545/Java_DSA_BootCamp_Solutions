549b9709b43b188a0f287e3db4449a151cab68ce
Vicky <vickygupta9987@gmail.com>
2021-12-04 12:51:43 +0530

    Solutions added

---
+++ 05-Arrays/Easy/PlusOne.java
@@ -0,0 +1,72 @@
+package com.company.Arrays_and_ArrayList.Assignment.Easy;
+
+/*
+//https://leetcode.com/problems/plus-one/
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+
+Example 1:
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Example 2:
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Example 3:
+Input: digits = [0]
+Output: [1]
+Explanation: The array represents the integer 0.
+Incrementing by one gives 0 + 1 = 1.
+Thus, the result should be [1].
+
+Example 4:
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+*/
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {4,3,2,1};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+    }
+    public static int[] plusOne(int[] digits) {
+        if(digits.length == 0) return digits;
+        for(int i = digits.length-1;i>=0;i--){
+            //if the last element in the array is smaller than 9 then we basically increment by 1 and return             //the digits array.
+            if(digits[i] < 9){
+                digits[i]++;
+
+                return digits;
+            }
+            //if the last element in the array is larger than 9 then after incrementing the element become
+            //become double digit element then we have to seperate the digits and place it
+            //into the array and then return.
+            digits[i] = 0;
+        }
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
+        return res;
+    }
+}
